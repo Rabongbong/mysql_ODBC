2917b9545022d94f70c8c81682dee3dfa51fd7e9
Heesoo <2016314167@swji.skku.edu>
2020-12-01 15:26:38 +0900

    [edit] DDL

---
+++ DDL.sql
@@ -0,0 +1,13 @@
+create table User(u_id	int AUTO_INCREMENT, name varchar(30), email	varchar(45) NOT NULL UNIQUE, password varchar(30), primary key (u_id));
+
+create table item(i_id	int AUTO_INCREMENT, u_id int, category varchar(20) check (category in ('Electronics', 'Books', 'Home', 'Clothing', 'Sporting Goods')), description varchar(50), conditions varchar(30) check (conditions in ('New', 'Like-New', 'Used (Good)', 'Used (Acceptable)')), buy_it_now_price numeric(8) , date_posted DATETIME not null default now(), bid_ending_date DATETIME, most_recent_bid_price numeric(8) default '0', primary key (i_id), foreign key (u_id) references User(u_id) on delete cascade);
+
+create table Transaction(transaction_id	int AUTO_INCREMENT, i_id	int, seller int, buyer int, sold_date DATETIME default now(), sold_price numeric(8), primary key (transaction_id), foreign key (i_id) references item(i_id) on delete set null, foreign key (seller) references User(u_id) on delete set null, foreign key (buyer) references User(u_id)	on delete set null);
+
+create table Watch_list( u_id	int, i_id	int, primary key (u_id, i_id), foreign key (u_id) references User(u_id) on delete cascade, foreign key (i_id) references item(i_id) on delete cascade);
+
+create table bid_history(u_id	int, i_id int, bid_price numeric(8,0), primary key (i_id, bid_price), foreign key (u_id) references User(u_id) on delete cascade, foreign key (i_id) references item(i_id)on delete cascade);
+
+create trigger transaction_buy after update on item for each row begin if new.most_recent_bid_price >= new.buy_it_now_price then insert into Transaction(i_id, seller, sold_date, sold_price) values(new.i_id, new.u_id, now(), new.most_recent_bid_price); end if; end;
+
+create trigger bid_money before update on item for each row begin if new.most_recent_bid_price < (select * from (select most_recent_bid_price from item where i_id=new.i_id) as W) then SIGNAL SQLSTATE '02000' SET MESSAGE_TEXT = 'Warning: you have to bid more money';end if; end;
--- connect.c
+++ connect.c
@@ -1025,63 +1025,6 @@ int main() {
 		exit(1);
 	}
 	
-
-	/*
-	char * trigger_transaction = "create trigger transaction_buy after update on item for each row begin if new.most_recent_bid_price >= new.buy_it_now_price then insert into Transaction(i_id, seller, sold_date, sold_price) values(new.i_id, new.u_id, now(), new.most_recent_bid_price); end if; end;";
-	char * trigger_bid = "create trigger bid_money before update on item for each row begin if new.most_recent_bid_price < (select * from (select most_recent_bid_price from item where i_id=new.i_id) as W) then SIGNAL SQLSTATE '02000' SET MESSAGE_TEXT = 'Warning: you have to bid more money';end if; end;";
-
-	if (mysql_query(conn, trigger_transaction)) {
-		fprintf(stderr, "%s\n", mysql_error(conn));
-		exit(1);
-	}
-	*//*
-	if (mysql_query(conn, trigger_bid)) {
-		fprintf(stderr, "%s\n", mysql_error(conn));
-		exit(1);
-	}
-	*/
-	/* send SQL query */
-	
-	/*
-	char * sql_user = "create table User(u_id	int AUTO_INCREMENT, name varchar(30), email	varchar(45) NOT NULL UNIQUE, password varchar(30), primary key (u_id))";
-	
-	char * sql_item = "create table item(i_id	int AUTO_INCREMENT, u_id int, category varchar(20) check (category in ('Electronics', 'Books', 'Home', 'Clothing', 'Sporting Goods')), description varchar(50), conditions varchar(30) check (conditions in ('New', 'Like-New', 'Used (Good)', 'Used (Acceptable)')), buy_it_now_price numeric(8) , date_posted DATETIME not null default now(), bid_ending_date DATETIME, most_recent_bid_price numeric(8) default '0', primary key (i_id), foreign key (u_id) references User(u_id) on delete cascade)";
-
-	char * sql_transaction = "create table Transaction(transaction_id	int AUTO_INCREMENT, i_id	int, seller int, buyer int, sold_date DATETIME default now(), sold_price numeric(8), primary key (transaction_id), foreign key (i_id) references item(i_id) on delete set null, foreign key (seller) references User(u_id) on delete set null, foreign key (buyer) references User(u_id)	on delete set null)";
-
-	char * sql_Watch = "create table Watch_list( u_id	int, i_id	int, primary key (u_id, i_id), foreign key (u_id) references User(u_id) on delete cascade, foreign key (i_id) references item(i_id) on delete cascade)";
-	
-	char * sql_bid = "create table bid_history(u_id	int, i_id int, bid_price numeric(8,0), primary key (i_id, bid_price), foreign key (u_id) references User(u_id) on delete cascade, foreign key (i_id) references item(i_id)on delete cascade)";
-
-	
-
-	if (mysql_query(conn, sql_user)) {
-		fprintf(stderr, "%s\n", mysql_error(conn));
-		exit(1);
-	}
-	
-	if (mysql_query(conn, sql_item)) {
-		fprintf(stderr, "%s\n", mysql_error(conn));
-		exit(1);
-	}
-
-	if (mysql_query(conn, sql_transaction)) {
-		fprintf(stderr, "%s\n", mysql_error(conn));
-		exit(1);
-	}
-
-	if (mysql_query(conn, sql_Watch)) {
-		fprintf(stderr, "%s\n", mysql_error(conn));
-		exit(1);
-	}
-
-	if (mysql_query(conn, sql_bid)) {
-		fprintf(stderr, "%s\n", mysql_error(conn));
-		exit(1);
-	}
-	
-	*/
-	
 	bid_ending();
 
 	while(1){
